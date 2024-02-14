6b7f01419064b15de56c722a0ea170771e3d18ca
Keven Ferraz de Almeida <114549244+Kevenferraz39@users.noreply.github.com>
2024-02-14 00:57:34 -0300

    modulo

---
+++ Programação-de-computadores/Modulo-I/teste-modulo/main.py
@@ -0,0 +1,8 @@
+# main.py
+from meumodulo import saudacao, soma
+
+nome = "Usuário"
+print(saudacao(nome))
+
+resultado = soma(1, 1)
+print(f"A soma é: {resultado}")
---
+++ Programação-de-computadores/Modulo-I/teste-modulo/meumodulo.py
@@ -0,0 +1,7 @@
+# meumodulo.py
+
+def saudacao(nome):
+    return f"Olá, {nome}!"
+
+def soma(a, b):
+    return a + b
