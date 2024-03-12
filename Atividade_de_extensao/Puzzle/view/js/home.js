7cd894c54faebabf5dd4f598c0cf20be19f7e476
Keven Ferraz de Almeida <114549244+Kevenferraz39@users.noreply.github.com>
2024-03-12 23:25:31 +0000

    at

--- Atividade_de_extensao/Puzzle/view/js/home.js
+++ Atividade_de_extensao/Puzzle/view/js/home.js
@@ -1,3 +1,13 @@
-function iniciar(){
-    
-}
+document.addEventListener("DOMContentLoaded", function() {
+    const imagens = [
+        {
+            img: "img/logo-2.png",
+            nome: "logo 2"
+        }
+    ];
+
+    var navbar = document.querySelector(".header-inner-content");
+    imagens.forEach((val) => {
+        navbar.innerHTML += `<img src="${val.img}" alt="${val.nome}" class="logoFoto">`;
+    });
+});
