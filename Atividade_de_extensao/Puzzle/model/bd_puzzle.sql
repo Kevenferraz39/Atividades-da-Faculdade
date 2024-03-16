87e81c117f20eb1724d04bf7c18daf1972f0bf8b
Keven Ferraz de Almeida <114549244+Kevenferraz39@users.noreply.github.com>
2024-03-16 13:01:43 -0300

    att

--- Atividade_de_extensao/Puzzle/model/bd_puzzle.sql
+++ Atividade_de_extensao/Puzzle/model/bd_puzzle.sql
@@ -1,8 +1,35 @@
 CREATE DATABASE bd_puzzle;
 USE bd_puzzle;
 
--- criacao da tabela usuarios adiministradores
+--  Criação da tabela para usuários administradores
+CREATE TABLE Administradores (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100),
+    email VARCHAR(100) UNIQUE,
+    senha VARCHAR(100)
+);
 
-CREATE TABLE Administradores(
+-- Criação da tabela para usuários com acesso aos exercícios
+CREATE TABLE Usuarios (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100),
+    email VARCHAR(100) UNIQUE,
+    senha VARCHAR(100)
+);
 
+-- Criação da tabela para os exercícios
+CREATE TABLE Exercicios (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100),
+    descricao TEXT
+);
+
+-- Criação da tabela para o progresso dos usuários nos exercícios
+CREATE TABLE Progresso (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    id_usuario INT,
+    id_exercicio INT,
+    data_conclusao DATE,
+    FOREIGN KEY (id_usuario) REFERENCES Usuarios(id),
+    FOREIGN KEY (id_exercicio) REFERENCES Exercicios(id)
 );
